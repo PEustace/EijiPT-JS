86ba1606561a54bec40c8384536a49c3501f3740
IAskOfYou <151792785+IAskOfYou@users.noreply.github.com>
2024-09-07 19:03:24 -0400

    Changing to type module

--- server/my_modules/chat.mjs
+++ server/my_modules/chat.mjs
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 import express from 'express';
 var router = express.Router();
-const client = new chat.OpenAI();
+const client = new OpenAI();
 //DO NOT LEAVE THIS IN WHEN PUBLIC
 const APIKey = "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"
 
