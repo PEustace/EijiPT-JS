ea4513a43f2a22681687d1669f376133185bdd75
IAskOfYou <iaskofyou77@gmail.com>
2024-09-13 14:14:10 -0400

    Commit

--- client/src/ChatPortal.jsx
+++ client/src/ChatPortal.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 async function CallChat(chatType, userText) {
-    const response = await axios.post("https://eustace.dev/api/chat", {
+    const response = await axios.post("https://eijipt.internal.jollyrock-0f3192be.eastus.azurecontainerapps.io:3000/api/chat", {
         chatType: chatType, //Type of chat, i.e. basic tutor or translator
         userText: userText}) //User's text entered
     .catch(error => {
