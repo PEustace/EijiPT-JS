4cad75ef190752be43b0f2d4c0345881cbe606bb
IAskOfYou <151792785+IAskOfYou@users.noreply.github.com>
2024-09-13 20:31:15 -0400

    Commit

--- client/src/ChatPage.jsx
+++ client/src/ChatPage.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import App from './App.jsx'
-//import ChatPortal from './ChatPortal.jsx'
+import ChatPortal from './ChatPortal.jsx'
 import './index.css'
 
 //console.log("Hello.");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <a href="../chatpage.html">Go To Chat</a>
+    <ChatPortal />
   </StrictMode>,
 )
