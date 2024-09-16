f84f7bdcd4f8c2f120981878c7c5e9e8c45b348b
IAskOfYou <151792785+IAskOfYou@users.noreply.github.com>
2024-09-16 14:56:45 -0400

    Commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, resolveConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolveConfig('/', 'index.html'),
+        chatpage: resolveConfig('/', '/chatpage/index.html')
+      }
+    }
+  },
   plugins: [react()],
 })
